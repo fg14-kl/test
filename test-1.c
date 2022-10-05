61c744a2887beb1538519dc13a3b24d64becb01c
fg 14-kl <3514596559@qq.com>
2022-10-05 11:25:55 +0800

    test

---
+++ test-1.c
@@ -0,0 +1,49 @@
+//int global=2030
+//void test()
+//{
+ // printf("test()--%d\n",global);
+//}
+//int main()
+//{
+  
+ // printf("%d\n",global);
+//int num=0;
+//printf("num=%d\n'num");
+ //int num1=0
+//  int num2=0
+ // int sum=0
+ // scanf("%d%d",&num1,num2);
+  //sum=num1+num2;
+  //printf("sun=%d\n",sun);
+   // printf("azaz\n");
+   // return 0;
+//short age=20;
+//float weight=94.2f;
+  // char ch='w';
+//printf(“%c\n",ch);
+//int age=20;
+//printf（"%d\n”，age）;
+//long num=100;
+//printf("%d\n",num);
+//float f=4.20;
+//printf("%f\n",f);
+//double d=4.51;
+//printf("%lf\n",d);
+//printf("%d\n",sizeof(chir));
+ //   return 0;
+//}
+//int main()
+//{
+ // extern int f-fff;
+ // printf("f-fff=%d\n",f-fff);
+//return 0
+//}
+#include<stdio.h>
+int main（）
+{
+{
+int a = 10
+printf("a=%d\n",a);
+}
+  return 0;
+}
