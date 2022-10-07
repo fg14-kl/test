7cfcc78e597db725453eca14d58648dc8bef9cf7
fg 14-kl <3514596559@qq.com>
2022-10-07 13:39:38 +0800

    test-3.c

---
+++ test-3.c
@@ -0,0 +1,276 @@
+#include<stdio.h>
+/*
+int main()
+{
+int input=0;
+printf("你要好好学习吗？(1/0)>:");
+scanf("%d",&input);
+if(input==1)
+printf("offer\n");
+else
+printf("mhs\n");
+    return 0;
+}
+*/
+
+
+// int main()
+// {
+//     int line = 0;
+// printf("加入比特\n");
+
+// while(line<200)
+// {
+//     printf("敲一行代码:%d\n",line);
+//     line++;
+// }
+// if(line>=200)
+// printf("好off\n");
+//     return 0;
+// }
+
+// int add (int x, int y)
+// {
+// int z = x+y；
+// return z;
+// }
+
+
+// int main()
+// {
+// int num1 = 10;
+// int num2 = 20;
+// int sum = 0;
+// int a = 2;
+// int b = 3;
+// //sum = num1 + num2;
+// sum = add (num1,num2)
+// //sum = a + b;
+// sum =add (a,b)
+// printf("sum = %d\n",sum);
+//     return 0;
+// }
+
+// int main()
+// {
+// int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+// int i=0;
+// while(i<10)
+// {
+// printf("%d",arr[i]);
+// i++;
+// }
+// return 0;
+// }
+
+// int main()
+// {
+// int a = 5%2;
+// printf("%d\n",a);
+//     return 0;
+// }
+
+// int main()
+// {
+//     int a=1;
+//     int b = a<<;
+//     printf("%d\n",b);
+//     return 0;
+// }
+
+// int main()
+// {
+// a += 10//a=a+10
+//     return 0
+// }
+
+// int main()
+// {
+// int a = 10;//非0--》真  ！--》反
+// printf("%d\n",!a);
+//     return 0;
+// }
+
+// int main()
+// {
+//     int zz = 0;
+// int arr[10] = {0};
+// printf("%d\n",sizeof(arr));
+// zz=sizeof(arr)/sizeof(arr[0]);
+// printf("%d\n",zz);
+// // int a = 10;
+// // printf("%d\n",sizeof(int));
+//     return 0;
+// }
+
+// int main()
+// {
+// int age = 10;
+// if(age<18)
+// {
+// printf("xxs\n");
+// printf("yyyyyyy\n");
+// }
+// else
+// {
+// if(age>=18 && age<45)
+// printf("cnr\n");
+// else if(age>=45 && age<90)
+// printf("qn\n");
+// else
+// printf("np\n");
+// }
+//     return 0;
+// }
+
+// int main()
+// {
+//     int a = 0;
+//     int b = 0;
+//     if(a==1)
+// {
+//     if(b==0)
+//     printf("huhu\n");
+// }
+//     else
+//     printf("uuuu\n");
+//     return 0;
+// }
+    //  =——》赋值  ==——》 5==x推荐
+
+    // int main()
+    // {
+    //     int a = 1;
+    //     while(a<=100)
+    //     {
+    //         //if(a%2==1)
+    //         printf("%d ",a);
+    //         a+=2;
+    //         //a++;
+    //     }
+    //     return 0;
+    // }
+        
+//         int main()
+//         {
+// int day = 0;
+// scanf("%d",&day);
+// switch(day)
+// {
+// case 1:
+// if(day==1)
+// printf("HEHE\n");
+// break;
+// case 3:
+// printf("xp1\n");
+// break;
+// case 2:
+// printf("xp2\n");
+// break;
+// default:
+// printf("nmd\a");
+// break;
+// }
+//             return 0;
+//         }
+
+// int main()
+// {
+// int n = 1;
+// int m = 2;
+// switch(n)
+// {
+// case 1: m++;
+// case 2: n++;
+// case 3:
+//  switch (n)
+//  {
+//  case 1:n++;
+//  case 2:m++;n++;break;
+//  }
+// case 4:m++;
+// break;
+// default:
+// break;
+// }
+// printf("m=%d,n=%d", m, n,);
+//     return 0;
+//}
+
+// int main()
+// {
+//     int a = 0;
+// while(a<=10)
+// {
+// a++;
+//     if(a == 5)
+//     continue;
+// printf("%d ",a);
+// }
+//     return 0;
+// }
+
+// int main()
+// {
+// //int ch = getchar();
+// // putchar(ch);
+// // printf("%c\n",ch);
+// int ch = 0;//ctrl+z
+// while((ch=getchar()) != EOF)//EOF------END OF FILE   即为 -1
+// {
+//     putchar(ch);
+// }
+//     return 0;
+// }
+
+
+// int main()
+// {
+// int ret = 0;
+// int ch = 0;
+// char passward[20] = {0};
+// printf("写密码:>");
+// scanf("%s", passward);
+// while ((ch=getchar()) !='\n')****
+// {****
+//     ;****
+// }****
+// printf("请确认(n/y):>");
+// ret = getchar();
+// if(ret == 'y')
+// {
+//     printf("chb\n");
+// }
+// else
+// {
+//     printf("fk\n");
+// }
+//     return 0;
+// }
+
+// /// @brief 
+// /// @return 
+// int main()
+// {
+// int ch = 0;
+// while((ch=getchar()) != EOF)
+// {
+//     if(ch<'0' || ch>'9')
+//     continue;
+//     putchar(ch);
+// }
+//     return 0;
+// }
+
+// int main()
+// {
+//     int i = 0;
+// for(i=1; i<=10; i++)
+// {
+//     if(i == 5)
+//     continue;
+//     printf("%d ", i);
+// }
+//     return 0;
+// }
+
