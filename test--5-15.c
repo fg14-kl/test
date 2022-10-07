304196847df83f2e367a5a514fcb6c7f12588f39
fg 14-kl <3514596559@qq.com>
2022-10-07 13:54:08 +0800

    test--5-15.c

---
+++ test--5-15.c
@@ -0,0 +1,40 @@
+
+void menu()
+{
+    printf("1 play,0 exit");
+}
+
+
+
+
+void test()
+{
+do
+{
+    int intput = 0;
+ menu();
+ printf("1/0");
+ scanf("%d",&intput);   /* code */
+ switch(intput);
+ {
+case1:
+ printf("play\n");
+break;
+printf("baba\n");
+case0:
+break;
+default:
+printf("aaaaaa\n");
+break;
+
+ }
+} while (/* condition */);
+
+
+}
+
+int main()
+{
+
+    return 0;
+}
