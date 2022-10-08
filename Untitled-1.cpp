c905d730f1dc2c2f0dc33ff61e5459efdfd7c2d5
fg 14-kl <3514596559@qq.com>
2022-10-08 15:30:52 +0800

    10-7

---
+++ Untitled-1.cpp
@@ -0,0 +1,128 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+//#define day 7
+int main()
+{
+//cout <<"hello world\n";
+//int a = 10;
+//cout << "a=" << a <<endl;
+/*cout <<"fsdfddfd"<<day<<"mnmnn"<<endl;
+const int month = 12;
+cout<<month<<endl*/
+//int NNN = 200;
+//cout << nnn << endl;
+/* short a = 10;  cout<<sizeof(a)<<endl;                                         //short -32768  32767
+int aa = 10;  cout<<sizeof(aa)<<endl;
+long ass = 10;  cout<<sizeof(ass)<<endl;
+long long as = 10;  cout<<sizeof(as)<<endl;
+float a1 = 3.14f;  cout<<a1<<endl;
+double a2 = 3.145;  cout<<a2<<endl;*/
+
+/*char bh = 'a';                                                                  //char - 一个字符 ''
+cout<<sizeof(bh)  <<   (int)bh<<bh<<endl;
+ string jj="hjjkkkh";                                                           //头文件  #include<sting>
+cout <<jj<<endl;
+cout <<"hello\tmnmn"<<endl;
+cout <<"hell\tmnmn"<<endl;
+cout <<"hel\tmnmn"<<endl;
+
+bool kko = true;
+cout<<kko<<sizeof(bool)<<endl;
+int a = 10;
+cin >> a;*/
+/*int j = 0;
+int k = 10;
+//j = k++ * 10 ; 
+j = ++k * 10 ;                                                                              //++k 先+后计算  ;;;   k++ 先计算后+
+cout<<k<<endl;
+cout<<j<<endl;*/
+
+/*int huhu = 0;
+cout << "请输入"<<endl;
+cin>>huhu;
+cout<<huhu<<endl;
+if(huhu>=500)
+{
+    cout<<"uiuiuiuiu"<<endl;
+}
+else if (huhu>=400 && huhu<500)
+
+{
+    cout<<"iuiu"<<endl;
+}
+
+else
+{
+    cout<<"njnjnjnjnjnjnjnjnjnjnjnjnj"<<endl;
+}*/
+
+//cout<<"请你输入"<<endl;
+/*int a,s,d;
+cin>>a;
+cin>>s;
+cin>>d;
+cout<<"a=  "<<a  <<"s=  "<<s  <<"d=   "<<d<<endl;
+ if(a>s)
+ {
+   if(a>d)
+   {
+    cout<<"max = a"<<endl;
+   }
+   else
+   {
+    cout<<"max = d"<<endl;
+   }
+ }
+else
+{
+    if(s>d)
+    {
+        cout<<"max = s"<<endl;
+    }
+    else
+    {
+        cout<<"max = d"<<endl;
+    }
+}*/
+
+/*int a =10;
+int b = 20;
+int c = 0;
+c = a>b ? a : b;                        //三目运算 
+cout<<c<<endl;，
+(a<b ? a:b ) = 100;
+cout<<a<<endl;
+cout<<b<<endl;*/
+              
+int m = 0;
+cin>>m;
+switch (m)                                    //switch 只运int
+{
+case 1:
+    cout<<"hjhjhj"<<endl;
+    break;
+case 3 :
+cout<<"huhu"<<endl;
+default:
+cout<<"j"<<endl;
+    break;
+}
+
+
+
+
+
+
+
+
+
+
+
+system("pause");
+
+
+return 0;
+}
